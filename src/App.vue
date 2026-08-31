7d71215dc5b48f542e0736fd08fda36181fe1bab
Dmitriy S <onlyzoran@gmail.com>
2026-08-31 19:51:49 +0300

    chore: init vue 3 + vite + typescript project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main class="app">
+    <h1>Tree Store</h1>
+  </main>
+</template>
+
+<style scoped>
+.app {
+  padding: 24px;
+  font-family: system-ui, sans-serif;
+}
+</style>
